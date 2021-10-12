21cbe4f8bbd190338b17d06abf05102eede4f571
Andrea Fazio <andrea.fazio@thelevelgroup.com>
2021-10-12 12:46:53 +0200

    final usestate basic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-import React from 'react';
 
 function App() {
-  let title='random title';
+  // useState('random title');
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // const {value,handler}=useState(1);
+  const [text,setText] = useState('random title')
   const handleClick = () => {
-    title = 'hello people'
+    // i call the function inside useState
+    (text === 'random title' && text.length !== 0) ? setText('hello people') : setText('random title')
   }
   return (
     <React.Fragment>
 
     <div className="container">
-      <h1>{title}</h1>
+      <h1>{text}</h1>
       <button type="Button" className='btn' onClick={handleClick}> Change title</button>
     </div>
     </React.Fragment>
