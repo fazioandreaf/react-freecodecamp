e26cb0e3649e1d3902f9b4cae897451359fea7b5
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-11-28 19:01:04 +0100

    Custom hok useFetch

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Modal from './Modal';
 import { data } from './data/data-setStateArray';
 import { reducer } from './reducer';
+import { useFetch } from './useFetch';
+
 
 function App() {
 	const defaultState = {};
 	const [people, setPeople] = useState(data);
 	const [state, dispatch] = useReducer(reducer, defaultState);
-	const [name,setName] = useState('');
+	const {loading, products} = useFetch('https://course-api.com/javascript-store-products');
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
@@ -19,7 +21,6 @@ function App() {
 			<div className='container'>
 					<h2>PropDrilling</h2>
 			</div>
-
 		</Fragment>
 	)
 }
