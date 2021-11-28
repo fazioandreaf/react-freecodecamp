62ca2a86e2b9977d1161e769db4518fba8abd688
Fazioandrea.f <fazioandrea.f@gmail.com>
2021-11-28 18:10:55 +0100

    Prop Drilling

--- src/PropDrilling.js
+++ src/PropDrilling.js
@@ -9,7 +9,6 @@ function App() {
 	const defaultState = {
 
 	}
-
 	const [people, setPeople] = useState(data);
 	const [state, dispatch] = useReducer(reducer, defaultState);
 	const [name,setName] = useState('');
@@ -18,16 +17,43 @@ function App() {
 		e.preventDefault();
 	}
 
+	const removePerson = (id) => {
+		setPeople((person)=>{
+			return person.filter((person) => person.id !== id);
+		})
+	}
+
+
 	return (
 		<Fragment>
 			<div className='container'>
 				<section>
 					<h2>PropDrilling</h2>
+						<List people={people} removePerson={removePerson} />
 				</section>
 			</div>
 
 		</Fragment>
 	)
+}
+
+const List = ({people, removePerson}) => {
+	return <>
+		{people.map((person) => {
+			return <SinglePerson key={person.id} {...person} removePerson={removePerson} />
+		})}
+	</>
+}
+
+const SinglePerson = ({id, name, removePerson}) => {
+	return <>
+		<div className='item'>
+			<h4>
+				{name}
+			</h4>
+			<button onClick={()=> removePerson(id)}>Remove</button>
+		</div>
+	</>
 }
 
 export default App;
